161bb4f4f8a52059cd08ca852b732f795e581ac2
fooloomanzii <hannes-b@web.de>
2016-10-27 09:21:39 +0200

    install tests

--- push.bat
+++ push.bat
@@ -1,14 +1,14 @@
 @echo off
 
 echo Performing Git Operations. This can take a moment ...
-git add .
 git submodule foreach "git add ."
+git add .
 echo.
 echo Enter a Message for the Commit
 set /p CommitMessage=
 echo.
-git commit -m "%CommitMessage%"
 git submodule foreach "git commit -m '%CommitMessage%'"
+git commit -m "%CommitMessage%"
 echo.
 git push --recurse-submodules=on-demand
 
