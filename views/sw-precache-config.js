35db57d913a2774ce7f15aac03f840d21bea6669
fooloomanzoo <hannes-b@web.de>
2017-07-28 17:01:06 +0200

    svg download try

--- views/sw-precache-config.js
+++ views/sw-precache-config.js
@@ -1,73 +1,90 @@
 module.exports = {
-  stripPrefix: '../views',
-  staticFileGlobs: [
-    'bower_components/**/*',
-    'src/**/*',
-    'scripts/**/*',
-    'fonts/**/*',
-    'icons/**/*',
-    'index.html'
-  ],
-  verbose: true,
-  navigateFallback: 'index.html',
-  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/$|\/images\/$|\/auth\/).*/],
-  runtimeCaching: [
-  {
-    urlPattern: /images\/.*/,
-    handler: 'cacheFirst',
-    options: {
-      cache: {
-        name: 'image-cache'
-      }
-    }
-  }, {
-    urlPattern: /data\/.*/,
-    handler: 'fastest',
-    options: {
-      cache: {
-        name: 'data-cache'
-      }
-    }
-  }, {
-    urlPattern: /fonts\/.*/,
-    handler: 'cacheFirst',
-    options: {
-      cache: {
-        name: 'data-cache'
-      }
-    }
-  }, {
-    urlPattern: /locales\/.*/,
-    handler: 'cacheFirst',
-    options: {
-      cache: {
-        name: 'locales-cache'
-      }
-    }
-  }, {
-    urlPattern: /icons\/.*/,
-    handler: 'cacheFirst',
-    options: {
-      cache: {
-        name: 'icons-cache'
-      }
-    }
-  }, {
-    urlPattern: /scripts\/.*/,
-    handler: 'fastest',
-    options: {
-      cache: {
-        name: 'scripts-cache'
-      }
-    }
-  }, {
-    urlPattern: /socket\.io\/socket\.io\.js/,
-    handler: 'fastest',
-    options: {
-      cache: {
-        maxEntries: 2,
-        name: 'socket.io'
-      }
-    }
-  }]
+	staticFileGlobs: [
+		'/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-loader.js',
+    '/bower_components/es6-promise/es6-promise.auto.min.js',
+		'/src/**/*',
+		'/scripts/**/*',
+		'/fonts/**/*',
+		'/icons/**/*',
+		'/images/**/*',
+		'/data/**/*'
+	],
+	runtimeCaching: [
+		{
+			urlPattern: /\/bower_components\/es6-promise\/.*.js/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'es6-polyfills-cache'
+				}
+			}
+		}, {
+			urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'webcomponentsjs-polyfills-cache'
+				}
+			}
+		}, {
+			urlPattern: /images\/.*/,
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'image-cache'
+				}
+			}
+		}, {
+			urlPattern: /data\/.*/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'data-cache'
+				}
+			}
+		}, {
+			urlPattern: /fonts\/.*/,
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'font-cache'
+				}
+			}
+		}, {
+			urlPattern: /locales\/.*/,
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'locales-cache'
+				}
+			}
+		}, {
+			urlPattern: /icons\/.*/,
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'icons-cache'
+				}
+			}
+		}, {
+			urlPattern: /scripts\/.*/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'scripts-cache'
+				}
+			}
+		}, {
+			urlPattern: /socket\.io\/socket\.io\.js/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					maxEntries: 2,
+					name: 'socket.io'
+				}
+			}
+		}
+	]
 };
